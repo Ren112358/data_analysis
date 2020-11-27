b155964a4dcf9878272aa81dc8b8fe3cfb3d0be2
Ren Masahiro <29018052+Ren112358@users.noreply.github.com>
2020-11-27 22:20:11 +0900

    [add] file to make data

---
+++ tda/src/make_data.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set(style='darkgrid')
+
+
+def main():
+    print('hello')
+
+if __name__ == '__main__':
+    main()
