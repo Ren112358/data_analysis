5955978f5117ce0d8723aecd81e54b7b4e9e7847
Ren Masahiro <29018052+Ren112358@users.noreply.github.com>
2020-12-09 22:07:47 +0900

    [add] random 2d data

--- tda/src/make_data.py
+++ tda/src/make_data.py
@@ -5,8 +5,23 @@ import seaborn as sns
 sns.set(style='darkgrid')
 
 
+def make_random_2d_data(n=100):
+    '''
+    --- arg(s) ---
+    n: int
+        number of data
+    '''
+    x = np.random.rand(n)
+    y = np.random.rand(n)
+    data = np.array([x, y])
+    np.savetxt(fname='../data/random_2d.txt', X=data)
+    plt.scatter(x, y)
+    plt.savefig('../fig/random_2d.png')
+
+
 def main():
-    print('hello')
+    make_random_2d_data()
+
 
 if __name__ == '__main__':
     main()
