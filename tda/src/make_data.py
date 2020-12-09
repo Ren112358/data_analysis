6ce8d4a93f787d71c04fb4c2ede560c914c10096
Ren Masahiro <29018052+Ren112358@users.noreply.github.com>
2020-12-09 22:12:15 +0900

    [add] 2d normal dist. data

--- tda/src/make_data.py
+++ tda/src/make_data.py
@@ -19,8 +19,27 @@ def make_random_2d_data(n=100):
     plt.savefig('../fig/random_2d.png')
 
 
-def main():
+def make_normal_dist_2d_data(n=100):
+    '''
+    --- arg(s) ---
+    n: int
+        number of data
+    '''
+    x = np.random.randn(n)
+    y = np.random.randn(n)
+    data = np.array([x, y])
+    np.savetxt(fname='../data/normal_dist_2d.txt', X=data)
+    plt.scatter(x, y)
+    plt.savefig('../fig/normal_dist_2d.png')
+
+
+def make_2d_data():
     make_random_2d_data()
+    make_normal_dist_2d_data()
+
+
+def main():
+    make_2d_data()
 
 
 if __name__ == '__main__':
